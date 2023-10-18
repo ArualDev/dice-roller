60452457d99040732f55eb664bd251dcd28876ba
Laura <62561398+ArualDev@users.noreply.github.com>
2023-10-18 12:49:10 +0200

    Format file

--- src/lib/animFunctions.ts
+++ src/lib/animFunctions.ts
@@ -22,11 +22,11 @@ export function easeInBounce(x: number): number {
 
 export function easeOutElastic(x: number): number {
     const c4 = (2 * Math.PI) / 3;
-    
+
     return x === 0
-      ? 0
-      : x === 1
-      ? 1
-      : Math.pow(2, -10 * x) * Math.sin((x * 10 - 0.75) * c4) + 1;
-    
-    }
+        ? 0
+        : x === 1
+            ? 1
+            : Math.pow(2, -10 * x) * Math.sin((x * 10 - 0.75) * c4) + 1;
+
+}
